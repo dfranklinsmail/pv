256edc22e0e52ba140ab8687b4a6e129e438e44b
Daniel Franklin <dfranklinsmail@gmail.com>
2018-04-16 23:20:47 -0400

    finally got the image data!

--- js/pContrast.js
+++ js/pContrast.js
@@ -21,15 +21,28 @@ $(document).ready(function () {
         //    var context =  element.getContext("2d");
         //    console.log(context);
         // });
+        var canvas = document.getElementsByTagName("canvas");
+        //var canvas = $('#canvas');
+        var arrayLength = canvas.arraylength;
+        for (var c of canvas) {
+            //var c = canvas[i];
+            var jCanvas = c.getContext('2d');
+            if (jCanvas !== null) {
+                console.log('the length of the canvas');
+                var cWidth = c.width;
+                console.log(cWidth);
+                console.log('the height of the canvas');
+                var cHeight = c.height;
+                console.log(cHeight);
+                console.log('jCanvas context');
+                console.log(jCanvas);
+                //console.log(jCanvas.getImageData());
+                var imgData = jCanvas.getImageData(0,0,cWidth, cHeight);
+                console.log(imgData);
 
-        var canvas = $('#canvas');
-        var jCanvas = canvas.getContext('2d');
-        console.log('jCanvas context');
-        console.log(jCanvas);
-        var textureCanvas = $(document).getElementById('canvas');
-        var context = textureCanvas.getContext('2');
-        console.log('canvas context')
-        console.log(context);
+            }
+        }
+       
     });
 
 });
