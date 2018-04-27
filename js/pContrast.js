f4af09a45fe65173168bcbd492668b23b5cb8895
Daniel Franklin <dfranklinsmail@gmail.com>
2018-04-26 23:36:42 -0400

    reduced the image to a count of the 256 colours

--- js/pContrast.js
+++ js/pContrast.js
@@ -1,32 +1,13 @@
 $(document).ready(function () {
 
     $('#calculate-contrast').click(function (event) {
-        // console.log("calculate contrast clicked!");
 
-        // console.log($('canvas elements '));
-        // var arrayOfCanvas = $('div#viewer canvas')
-        // console.log(arrayOfCanvas);
-        // console.log('first element ');
-        // var firstElement = arrayOfCanvas[0];
-        // console.log(firstElement);
-        // console.log('context');
-        // var context = firstElement.getContext('2d');
-        // console.log(context);
-        // console.log('imagedata ');
-        // var imageData = firstElement.imageData;
-        // console.log(imageData);
-       
-        // $('div#viewer canvas').forEach(element => {
-            
-        //    var context =  element.getContext("2d");
-        //    console.log(context);
-        // });
         var canvas = document.getElementsByTagName("canvas");
         //var canvas = $('#canvas');
         var arrayLength = canvas.arraylength;
         for (var c of canvas) {
             //var c = canvas[i];
-            var jCanvas = c.getContext('2d');
+            var jCanvas = c.getContext('webgl');
             if (jCanvas !== null) {
                 console.log('the length of the canvas');
                 var cWidth = c.width;
@@ -36,13 +17,51 @@ $(document).ready(function () {
                 console.log(cHeight);
                 console.log('jCanvas context');
                 console.log(jCanvas);
-                //console.log(jCanvas.getImageData());
-                var imgData = jCanvas.getImageData(0,0,cWidth, cHeight);
-                console.log(imgData);
 
+                var pixels = processData(0, 0, cWidth, cHeight, jCanvas);
+
+                var colourNumbersCount = countColours(pixels);
+
+                displayColourNumbercount(colourNumbersCount);
             }
         }
        
     });
 
+
+
+
+
+
 });
+
+
+function processData (x, y, width, height, webGlContext) {
+    var pixels = new Uint8Array(width * height * 4);
+    webGlContext.readPixels(0, 0, width, height, webGlContext.RGBA, webGlContext.UNSIGNED_BYTE, pixels);
+    console.log(pixels);
+
+    return pixels;
+};
+
+function countColours (unit8Array) {
+    var counts = new Array(255);
+    for (var i in unit8Array) {
+        if (isNaN(counts[unit8Array[i]])) {
+            counts[unit8Array[i]] = 1;
+        } else {
+            counts[unit8Array[i]] = counts[unit8Array[i]] + 1;
+        }
+    }
+
+    return counts;
+};
+
+function displayColourNumbercount (colourCounts) {
+    console.log('The colour counts are :');
+    for(var i = 0; i < colourCounts.arrayLength; i++) {
+
+        str = i + ' : ' + colourcounts[i];
+        console.log(str);
+    }
+};
