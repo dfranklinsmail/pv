d4893526131e6d91fd25a1fe16122668c5f7a76f
Daniel <dfranklinsmail@gmail.com>
2018-07-31 15:56:47 -0400

    started hill climbing algorithm

---
+++ js/HillClimb.js
@@ -0,0 +1,73 @@
+class HillClimb {
+
+    constructor() {
+        //this.move = new Moves();
+        //var loopId = setInterval(step, 1000);
+        this.moves = new Moves();
+        this.loopId = null;
+
+    };
+
+   climb() {
+        console.log('climbing....');
+        var t = this;
+        this.loopId = setInterval(function() {t.step();}, 1000);
+       
+    } 
+
+    step() {
+        var move = this.moves.next();
+        if (move == null) {
+            //if the current is less then best neighbour
+            //then:
+                //move to best 
+                // recalculate moves
+            //else
+                //end loop
+            clearInterval(this.loopId);
+        } else {
+            //TODO: need to split this up into two!!!!
+
+            //a nearest neighbour exists
+            //move to it, 
+            //calculate its value
+                //if this value is greater then the current
+                //values set the current value to this value
+                //and the current move to this move
+            //move back to where you came
+            console.log(move);
+        }
+    }
+}
+
+class Moves {
+    constructor() {
+        this.currentIndex = 0;
+        this.moves = this.calculateMoves();
+    }
+
+    next() {
+        if (this.currentIndex < this.moves.length) {
+            return this.moves[this.currentIndex++];
+        } else {
+            return null;
+        }
+    }
+
+    calculateMoves() {
+        this.currentIndex = 0;
+        return [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            [-1, 0, 0],
+            [0, -1, 0],
+            [0, 0, -1]
+        ];
+    }
+}
+
+//function test() {
+    var hillClimb = new HillClimb();
+    hillClimb.climb();
+//}
