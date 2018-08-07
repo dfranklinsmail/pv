72c9108c31104ba615360ad79560aff286254a62
Daniel <dfranklinsmail@gmail.com>
2018-08-06 22:36:40 -0400

    hill climb integration with gui

--- js/HillClimb.js
+++ js/HillClimb.js
@@ -1,38 +1,72 @@
 'use strict';
 
-
 class HillClimb {
 
     constructor(Model) { //constructor could take a model object?
-        
-        //this.move = new Moves();
-        //var loopId = setInterval(step, 1000);
+        //helpers variables
         this.moves = new Moves();
         this.model = Model;
+
+        //loop variables
         this.loopId = null;
 
+        //step variables
+        this.localMaxValue = 0.0;
+        this.localMaxOrientation = null;
+
+        this.maxNeighbourValue = 0.0;
+        this.maxNeighbourMove = null;
+
+        this.currentMove = null;
     };
 
    climb() {
         console.log('climbing....');
-        var t = this;
+        var t = this;  
         this.loopId = setInterval(function() {t.step();}, 1000);
-       
     } 
 
     step() {
-        var move = this.moves.next();
-        if (move == null) {
-            //if the current is less then best neighbour
-            //then:
-                //move to best 
-                // recalculate moves
-            //else
+        if (this.moves.isempty()) {
+            //if the known localMax is less then max neighbour
+            if (this.localMaxValue < this.maxNeighbourValue) {
+                //then:
+                 // recalculate moves
+                this.moves.calculateMoves();
+                //set the new localMax
+                this.localMaxValue = this.maxNeighbourValue;
+                //move to local max
+                this.model.rotate(this.maxNeighbourMove[0], this.maxNeighbourMove[1], this.maxNeighbourMove[2]);
+                this.localMaxOrientation = this.model.currentRotation;
+            } else {
                 //end loop
-            clearInterval(this.loopId);
+                clearInterval(this.loopId);
+            }
         } else {
             //TODO: need to split this up into two!!!!
-
+            if (this.currentMove === null) {
+                this.currentMove = this.moves.peak();
+                console.log('moving to new neighbour');
+                console.log(this.currentMove);
+                this.model.rotate(this.currentMove[0], this.currentMove[1], this.currentMove[2]);
+            } else {
+                //remove a move
+                var cMove = this.moves.pop();
+                //
+                console.log('calculate and move back');
+                var cValue = this.model.calculateValue();
+                console.log('cValue ' + cValue);
+                if (cValue > this.maxNeighbourValue) {
+                    this.maxNeighbourValue = cValue;
+                    this.maxNeighbourMove = cMove;
+                }
+                
+                var negativeMove = this.moves.negate(cMove);
+                this.model.rotate(negativeMove[0], negativeMove[1], negativeMove[2]);
+                //reset
+                this.currentMove = null;
+                console.log('moved back');
+            }
             //a nearest neighbour exists
             //move to it, 
             //calculate its value
@@ -40,7 +74,8 @@ class HillClimb {
                 //values set the current value to this value
                 //and the current move to this move
             //move back to where you came
-            console.log(move);
+            //console.log(move);
+            //this.model.rotate(move[0], move[1], move[2]);
         }
     }
 };
@@ -51,12 +86,24 @@ class Moves {
         this.moves = this.calculateMoves();
     }
 
-    next() {
+    pop() {
         if (this.currentIndex < this.moves.length) {
             return this.moves[this.currentIndex++];
         } else {
             return null;
         }
+    }
+
+    peak() {
+        if (this.currentIndex < this.moves.length) {
+            return this.moves[this.currentIndex];
+        } else {
+            return null;
+        }
+    }
+
+    isempty() {
+        return this.currentIndex >= this.moves.length;
     }
 
     calculateMoves() {
@@ -70,11 +117,14 @@ class Moves {
             [0, 0, -1]
         ];
     }
+
+    negate(move) {
+        return move.map(value => -value);
+    }
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
     module.exports = HillClimb;
   else
-    window.HillClimb = HillClimb;
-
+window.HillClimb = HillClimb;
 
--- js/PVModel.js
+++ js/PVModel.js
@@ -12,14 +12,29 @@ class Model {
         this.currentRotation[6] = 0;
         this. currentRotation[7] = 0;
         this. currentRotation[8] = 1;
+
+        this.maxBlue = 400000;
+        this.maxRed = 400000;
+        this.maxGreen = 400000;
     }
 
     rotate(xDegree, yDegree, zDegree) {
+        //var myPV = require(['pv']);
+        // var pvNew = window.pv;
+        // var v = myPV.viewer;
+        // this.currentRotation = rotateZ(rotateY(rotateX(this.currentRotation, xDegree), yDegree), zDegree);
+        // console.log('current rotation');
+        // console.log(this.currentRotation);
+        // v.setRotation(this.currentRotation, 0);
+
+        this.currentRotation = rotateZ(rotateY(rotateX(this.currentRotation, xDegree), yDegree), zDegree);
+        console.log('current rotation');
+        console.log(this.currentRotation);
+        var cr = this.currentRotation
+        var myPV;
         require(['pv'], function(PV) {
-            pv = PV;
-            viwer = pv.viewer;
-            this.currentRotation = rotateZ(rotateY(rotateX(this.currentRotation, xDegree), yDegree), zDegree);
-            viewer.setRotation(this.currentRotation, 0);
+            myPV = PV;
+            viewer.setRotation(cr, 0);
         })
     }
 
@@ -106,7 +121,111 @@ class Model {
 
         return result;
     }
-};
+
+    calculateValue() {
+        var canvas = extractCanvas();
+        var pixels = processData(canvas);
+        return extractColours(pixels);
+    }
+
+    extractCanvas() {
+        var canvas = document.getElementsByTagName("canvas");
+
+        var arrayLength = canvas.arraylength;
+        for (var c of canvas) {
+            var webglContext = c.getContext('webgl');
+            var twoDContext = c.getContext('2d');
+            if (webglContext !== null) {
+                return c;
+            }
+        }
+    }
+
+    processData (canvas) {
+        var width = canvas.width;
+        var height = canvas.height;
+        var webGLContext = canvas.getContext('webgl');
+        var pixels = new Uint8Array(width * height * 4);
+        console.log('reading pixels');
+        webGLContext.readPixels(0, 0, width, height, webGLContext.RGBA, webGLContext.UNSIGNED_BYTE, pixels);
+        return pixels;
+    }
+
+ 
+
+    extractColours(pixels) {
+        var hexColour = 0;
+        var colour = "";
+        var redCount = 0;
+        var blueCount = 0;
+        var greenCount = 0;
+        var noColourCount = 0;
+        for (i = 0; i < pixels.length; i = i + 4) { 
+            colour = colourName(pixels[i], pixels[i+1], pixels[i+2]);
+            
+            if (colour == 'red') {
+                redCount++;
+            }
+            if (colour == 'blue') {
+                blueCount++;
+            }
+            if (colour == 'green') {
+                greenCount++;
+            }
+            if (!colour) {
+                noColourCount++;
+            }
+        }
+        console.log('blue count is '+blueCount);
+        console.log(blueCount);
+        console.log('red count is '+ redCount);
+        console.log(redCount);
+        console.log('green count is '+greenCount);
+        console.log(greenCount);
+        console.log('no Colour count is '+ noColourCount);
+        console.log(noColourCount);
+
+        var total = blueCount + redCount + greenCount;
+        console.log('the total is ' +total);
+        console.log(total);
+
+        //TODO max average count
+        var maxAvgBlue = blueCount/this.maxBlue;
+        console.log('max average blue count is '+maxAvgBlue);
+        console.log(maxAvgBlue);
+        var maxAvgGreen = greenCount/this.maxGreen;
+        console.log('max average green count is '+maxAvgGreen);
+        console.log(maxAvgGreen);
+        var maxAvgRed = redCount/this.maxRed;
+        console.log('max average red count is '+maxAvgRed);
+        console.log(maxAvgRed);
+        var totalAvgColour = maxAvgBlue + maxAvgGreen + maxAvgRed;
+
+        console.log('the total average colour count is '+totalAvgColour);
+        console.log(totalAvgColour);
+        return totalAvgColour;
+    }
+
+    colourName(r, g, b) {
+        var colour = false;
+
+        if (r == 0 & g == 0 & b == 0) {
+            colour = 'black';
+        } else if (r == 255 & g == 255 & b == 255) {
+            colour = 'white';
+        } else if (r === g & b === g) {
+            colour = 'gray';
+        } else if (r > g & r > b) { 
+            colour = 'red';
+        }  else if (g > r & g > b) { 
+            colour = 'green';
+        }  else if (b > r & b > g) { 
+            colour = 'blue';
+        }
+        return colour;
+    }
+    
+}   
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
     module.exports = Model;
